44df5fd5c01952d4097ee5e4cf8ee89160bf3751
cyubit <ahaba066@uottawa.ca>
2022-07-21 07:06:01 -0400

    navbar component

---
+++ bellissimo-cars/src/NavBar.js
@@ -0,0 +1,17 @@
+function NavBar() {
+    return (
+      <div className="navbar">
+            <nav className="navbar">
+                <ul>
+                    <li><a href="events.html"><h2>Events</h2></a></li>
+                    <li><a href="gallery.html"><h2>Models</h2></a></li>
+                    <li><a href="index.html"><h1>Bellissimo</h1></a></li>
+                    <li><a href="dealers.html"><h2>Dealers</h2></a></li>
+                    <li><a href="faq.html"><h2>FAQ</h2></a></li>
+                </ul>
+            </nav>
+      </div>
+    );
+  }
+  
+  export default NavBar;
